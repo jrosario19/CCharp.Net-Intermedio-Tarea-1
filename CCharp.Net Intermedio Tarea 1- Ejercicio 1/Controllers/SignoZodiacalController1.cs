fb7a41949e3c9a863fd3093cdc0be4a37367f7f5
jrosario19@hotmail.com <jrosario19@hotmail.com>
2020-01-27 17:57:31 -0400

    Agregar archivos de proyecto.

---
+++ CCharp.Net Intermedio Tarea 1- Ejercicio 1/Controllers/SignoZodiacalController1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CCharp.Net_Intermedio_Tarea_1__Ejercicio_1.Controllers
+{
+    public class SignoZodiacalController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ CCharp.Net Intermedio Tarea 1- Ejercicio 1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CCharp.Net_Intermedio_Tarea_1__Ejercicio_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
